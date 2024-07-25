c3cd2545052aae804474f21c4013fd0ad7dfcd2f
Ayush Gangwar <ayushgangwar3011@gmail.com>
2024-07-26 00:21:09 +0530

    add traversals and functions

--- binaryTree.cpp
+++ binaryTree.cpp
@@ -65,6 +65,92 @@ void postOrder(struct Node *p){
     }
 }
 
+void IPreorder(struct Node *p)
+{
+    struct Stack stk;
+    stackCreate(&stk, 100);
+
+    while (p || !isEmptyStack(stk))
+    {
+        if (p)
+        {
+            cout << p->data << " ";
+            push(&stk, p);
+            p = p->lchild;
+        }
+        else
+        {
+            p = pop(&stk);
+            p = p->rchild;
+        }
+    }
+}
+
+void IInorder(struct Node *p)
+{
+    struct Stack stk;
+    stackCreate(&stk, 100);
+
+    while (p || !isEmptyStack(stk))
+    {
+        if (p)
+        {
+            push(&stk, p);
+            p = p->lchild;
+        }
+        else
+        {
+            p = pop(&stk);
+            if (p)
+            {
+                cout << p->data << " ";
+                p = p->rchild;
+            }
+        }
+    }
+}
+
+void LevelOrder(struct Node *root)
+{
+    struct Queue q;
+    create(&q, 100);
+    cout << root->data<<" ";
+    enqueue(&q, root);
+
+    while (!isEmpty(q))
+    {
+        root = dequeue(&q);
+        if (root->lchild)
+        {
+            cout << root->lchild->data<<" ";
+            enqueue(&q, root->lchild);
+        }
+        if (root->rchild)
+        {
+            cout << root->rchild->data<<" ";
+            enqueue(&q, root->rchild);
+        }
+    }
+}
+
+int count(struct Node* root){
+    if(root)
+        return count(root->lchild) + count(root->rchild) + 1;
+    return 0;
+}
+
+int height(struct Node* root){
+    int x = 0, y = 0;
+    if(root==0)
+        return 0;
+    x = height(root->lchild);
+    y = height(root->rchild);
+    if(x>y)
+        return x + 1;
+    else
+        return y + 1;
+}
+
 int main(){
     Tcreate();
     preOrder(root);
